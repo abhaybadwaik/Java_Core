1caf0581814213b055960c8a48208ee3f0675f62
abhaybadwaik <badwaikabhay@gmail.com>
2025-11-07 12:48:03 +0530

    Stream problem

--- streams/StreamExamples.java
+++ streams/StreamExamples.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class StreamExamples {
     public static void main(String[] args) {
     StreamExamples obj = new StreamExamples();
-    obj.even();
+//    obj.even();
+    obj.odd();
     }
     void even(){
         List<Integer> list = Arrays.asList(1,5,4,8,2,6);
@@ -14,5 +15,12 @@ public class StreamExamples {
                 .filter(n-> n%2==0)
                 .sorted()
                 .forEach(System.out::println);
+    }
+    void odd(){
+        List<Integer> numbs = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        numbs.stream()
+                .filter(n->n%2!=0)
+                .forEach(System.out::println);
+
     }
 }
